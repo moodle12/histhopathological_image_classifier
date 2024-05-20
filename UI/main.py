9ce99449697066a7c485fb93c45e967dc3a0e56b
Mahmood Topiwala <103280407+moodle12@users.noreply.github.com>
2024-05-20 21:53:55 +0530

    completed the pruning and quantization with docker

--- UI/main.py
+++ UI/main.py
@@ -7,9 +7,9 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import base64
 from fastapi.staticfiles import StaticFiles
+from skimage import color
 
 app = FastAPI()
-# Assume the 'MODEL' variable is already defined with the loaded model
 MODEL = tf.keras.models.load_model('model4.h5')
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -21,7 +21,7 @@ async def read_root():
 def preprocess_input_image(image_path):
     # Load and preprocess the input image for the model
     img = cv2.imread(image_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (512, 512))
     img = img / 255.0  # Normalize pixel values to [0, 1]
     img = np.expand_dims(img, axis=0)  # Add batch dimension
@@ -48,8 +48,14 @@ async def predict_image(file: UploadFile = File(...)):
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
         # Preprocess the image for prediction
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        read = img
+        # img = cv2.cvtColor(img, cv2)
+        # read_rgb=img.copy()
+        read = img.copy()
+        # Increase brightness (make colors lighter)
+        # read[:, :, 1] *= 2.5  # Increase brightness of Eosin channel
+        # read[:, :, 2] *= 1.5  # Increase brightness of DAB channel
+        # Clip values to ensure they are within the valid range
+        # read = np.clip(read, 0, 1)
         img = cv2.resize(img, (512, 512))
         img = img / 255.0  # Normalize pixel values to [0, 1]
         img = np.expand_dims(img, axis=0)  # Add batch dimension
@@ -68,39 +74,3 @@ async def predict_image(file: UploadFile = File(...)):
         return {"error": str(e)}
 
 
-# @app.post("/predict")
-# async def predict_image(file: UploadFile = File(...)):
-#     try:
-#         # Read the uploaded image
-#         contents = await file.read()
-#         nparr = np.fromstring(contents, np.uint8)
-#         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-#         # Preprocess the image
-#         desired_width, desired_height = 512, 512
-#         input_image = cv2.resize(img, (desired_width, desired_height))
-#         input_image = input_image / 255.0
-#         input_image = np.expand_dims(input_image, axis=0)
-
-#         # Use model.predict to obtain the predicted mask
-#         predicted_mask = MODEL.predict(input_image)
-
-#         # Visualize the original image and predicted mask (optional)
-#         plt.subplot(1, 2, 1)
-#         plt.imshow(img)
-#         plt.title('Original Image')
-#         plt.axis('off')
-
-#         plt.subplot(1, 2, 2)
-#         plt.imshow(predicted_mask[0, :, :, 0], cmap='gray')
-#         plt.title('Predicted Mask')
-#         plt.axis('off')
-
-#         plt.show()
-
-#         # You can return the predicted mask as a response
-#         return {"prediction": predicted_mask.tolist()}
-
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
